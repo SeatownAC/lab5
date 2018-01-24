fe330fe32b2dee87349dfe7e12084d474d6bfccd
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-01-23 23:27:21 -0800

    update

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ testSumAndMultiply(4,7,5);
 /* Problem 4
 
 
+
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+
+
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
@@ -90,10 +94,23 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) {
-    
   //eslint-disable-line
 }
-sumArray(2, 3, 4);
+var num = 0
+function sumArray(newArray) {
+  for (var i = 0; i < newArray.length; i++) {
+
+    var totalSumArray = sum(num, newArray[i])[0];
+    num = totalSumArray;
+  }
+  
+  var msg = newArray[0] + ',' + newArray[1] + ',' + newArray[2] +'  was passed in as an array of numbers and ' + totalSumArray + ' is their sum.';
+  }
+
+  sumArray(2,3,4);
+
+  
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
